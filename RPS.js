aa51eedd5be97f4d67534caa2a791d3eb1edb5f0
nerdyjulz <122071416+nerdyjulz@users.noreply.github.com>
2023-04-07 21:52:33 +1000

    add JS Rock Paper Scissor game (in Dev Console)

---
+++ RPS.js
@@ -0,0 +1,107 @@
+// function to return Computer's choice (random) of Rock, Paper or Scissors (RPS)
+function getComputerChoice(){
+    let randNum = Math.ceil(Math.random() * 3);
+    //console.log(randNum);
+    let compChoice = "";
+    if (randNum === 1) {
+        compChoice = "ROCK"
+    } else if (randNum === 2) {
+        compChoice = "SCISSOR"
+    } else {
+        compChoice = "PAPER"
+    }
+    //console.log("Comp choice: " + compChoice);
+    return compChoice;
+}
+
+// function to return User's choice of Rock, Paper or Scissors (RPS) via prompt
+function getUserChoice(){
+    let userChoice = prompt("What do you choose: Rock, Paper or Scissors?").toUpperCase();
+    //console.log("First Input: " + userChoice);
+    while ((userChoice !== "ROCK") && (userChoice !== "PAPER") && (userChoice !== "SCISSOR")) {
+        userChoice = prompt("Incorrect input, please choose: Rock, Paper or Scissors!").toUpperCase();
+        //console.log("Other Input: " + userChoice);
+    }
+    //console.log("You're free with user input: " + userChoice);
+    return userChoice;
+}
+// function to play single round of RPS, taking user and computer input, 
+// and returning string which announces result.
+// Note: Make user input parameter case-insensitive.
+function playRound(humanInput="error", computerInput="error"){
+    let user = humanInput;
+    //console.log("User input is "+ user);
+    let comp = computerInput;
+    //console.log("Computer input is "+ comp);
+    let result = "";
+    //win message + count
+    function userWin(){
+        result = "You WIN this round :)";
+        userWinCount = userWinCount+1;
+    }
+    //lose message + count
+    function compWin(){
+        result = "You LOSE this round :(";
+        compWinCount = compWinCount+1;
+    }
+
+    if (user===comp) {
+        result = "It's a TIE! Go again!";
+        tieCount = tieCount + 1;
+        //console.log(result);
+    } else if (user === "PAPER") {
+        if (comp === "ROCK"){
+            userWin();
+        } else if (comp === "SCISSOR"){
+            compWin();
+        }          
+    } else if (user === "SCISSOR") {
+        if (comp === "PAPER"){
+            userWin();
+        } else if (comp === "ROCK"){
+            compWin();
+        }          
+    } else if (user === "ROCK") {
+        if (comp === "SCISSOR"){
+            userWin();
+        } else if (comp === "PAPER"){
+            compWin();
+        }          
+    }
+    console.log("You selected " + user + " and the Computer selected " + comp + ". " +result);
+}
+
+// main function to play 5 rounds of RPS.
+// tip: use console.log() to display result of each round and final winner. 
+function playGame() {
+    for (let x = 0; x < 5; x++) {
+        //console.log ("value of X is " + x);
+        //console.log("The result of game " + (x+1) + " is :")
+        playRound(getUserChoice(), getComputerChoice());
+        if (tieCount === 1){
+            tieCount = tieCount - 1;
+            x = x - 1;
+        } else {
+            totalGames = userWinCount + compWinCount;
+            console.log("Current score after " + totalGames + " games is User:" + userWinCount + " and Computer:" + compWinCount + ".");
+            if (totalGames === 5) {
+                if (compWinCount > userWinCount) {
+                    console.log ("You lost the match to the COMPUTER!! OH NO :(");
+                } else {
+                    console.log ("WOOHOO!! YOU WON!!");
+                }
+            }
+        }
+    }
+}
+
+// initialise variables and call Rock Paper Scissor game function.
+let userWinCount = 0;
+let compWinCount = 0;
+let tieCount = 0;
+let totalGames = 0;
+console.log ("Your Rock Paper Scissor Fight, Best of 5, starts NOW!!");
+playGame();
+
+
+
