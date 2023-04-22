02deedb694b43fe922643b009eb8e078519cd641
nerdyjulz <122071416+nerdyjulz@users.noreply.github.com>
2023-04-22 13:43:30 +1000

    add mouseover functionality to buttons

--- RPS.js
+++ RPS.js
@@ -5,6 +5,7 @@ let tieCount = 0;
 let totalGames = 0;
 let gameFinished = 0;
 let matchCount = 0;
+let timeout = 10000;
 const buttonDiv = document.querySelector('#buttons');
 const buttons = document.querySelectorAll('button');
 const paperButton = document.querySelector('.paper');
@@ -18,7 +19,7 @@ console.log ("Your Rock Paper Scissor Fight, Best of 5, starts NOW!!");
 const welcomeDiv = document.createElement('div');
 buttonDiv.insertAdjacentElement('beforebegin',welcomeDiv);
 welcomeDiv.setAttribute('id', 'welcome');
-welcomeDiv.innerText = "Welcome to the ROCK, PAPER, SCISSOR game! It's a best of 5 match - click any of the buttons below to commence a round.";
+welcomeDiv.innerText = "Welcome to the ROCK, PAPER, SCISSOR game! \n\n The match is Best-of-5 format...Good Luck! \n\n (Click the buttons to start playing...)";
 
 //Initialise additional Button Div elements
 const resetButton = document.createElement('button');
@@ -27,10 +28,13 @@ resetButton.className = "reset";
 //initialise sub-sections of the result div (i.e. round/match results)
 const resultDiv = document.createElement('div');
 const roundResultPara = document.createElement('p');
-const matchResultPara = document.createElement('p');
+const matchResultDiv = document.createElement('div');
 roundResultPara.className = "roundResult";
+matchResultDiv.className = "matchResult";
 resultDiv.className = "results";
 buttonDiv.insertAdjacentElement('afterend',resultDiv);
+resultDiv.insertAdjacentElement('afterend',matchResultDiv);
+matchResultDiv.style.display = 'none';
 
 // return Computer's choice (random) of Rock, Paper or Scissors (RPS)
 function getComputerChoice(){
@@ -50,7 +54,7 @@ function getComputerChoice(){
 function getUserChoice(x){
     //v2 mod - no longer takes user prompt, but inputs from button click
     let userChoice = x.toUpperCase();
-    console.log(typeof(userChoice)+ ': '+userChoice);
+    //console.log(typeof(userChoice)+ ': '+userChoice);
     return userChoice;
 }
 // Play a single round of RPS, taking user and computer input, 
@@ -96,15 +100,16 @@ function playRPSRound(humanInput="error", computerInput="error"){
     }
     resultDiv.appendChild(roundResultPara);
     totalGames = userWinCount + compWinCount;
-    roundResultPara.innerText = "Total Games: "+totalGames+ ". Your choice: " + userInput + " vs. Computer choice: " + compInput + ". " +result +" UserWin: " + userWinCount +". CompWin: " + compWinCount + ". ";
+    roundResultPara.innerText = "Round #"+totalGames+ " \n\nYour choice: " + userInput + "\nComputer choice: " + compInput + "\n\n " +result +"\n\n UserWin: " + userWinCount +". CompWin: " + compWinCount + ". ";
 }
 
 //function to announce winner, depending on who has more wins
 function announceWinner(){
+    matchResultDiv.style.display = 'inline';
     if (userWinCount > compWinCount) {
-        return "You Won!! Great job!! Man > AI!";
+        return "YOU WON!! \n Man shall not be controlled by AI! \n\n (resetting in " + (timeout / 1000) + " seconds...)";
     } else {
-        return "Noooo!! You Lost!! The AI will take over the world!";
+        return "YOU LOST!! \n The AI will take over the world! \n\n (resetting in )" + (timeout / 1000) + " seconds...)";
     }
 }
 
@@ -122,7 +127,7 @@ function viewReset(){
     paperButton.style.display = 'inline';
     scissorButton.style.display = 'inline';
     rockButton.style.display = 'inline';
-    matchResultPara.innerText = "";
+    matchResultDiv.innerText = "";
     roundResultPara.innerText = "";
 
     // set tracking variables to default values
@@ -140,26 +145,30 @@ function viewReset(){
     /* RPSv2: Step2: Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked */
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
+            //Manage mouse-over button interaction
+            console.log("Mouse-clicked with ClassName: "+ button.className);
+            if (button.className.includes(" playing") ){
+                button.className = button.className.slice(0, (button.className.length - 8));
+            }
+            console.log("Button post-slice ClassName: "+ button.className);
             console.log("In Match #: " + matchCount + " game #: " + totalGames);
             const buttonName = button.className;
-            console.log (buttonName);
+            //console.log (buttonName);
             playRPSRound(getUserChoice(buttonName),getComputerChoice());
             //Once 5 games reached, announce winner and reset view
             if (totalGames === 5) {
                 console.log ("5 GAMES STOP PLAY");
-    /*          console.log(buttons);
-                console.log (buttons[0]);
-                console.log (buttons[1]);
-                console.log (buttons[2]); */
                 paperButton.disabled = true;
                 scissorButton.disabled = true;
                 rockButton.disabled = true;
                 
                 // RPSv2: Step4: Display the running score, and announce a winner of the game once one player reaches 5 points.
                 //display the result in a new paragraph
-                matchResultPara.innerText = announceWinner();
-                resultDiv.appendChild(matchResultPara); 
+                matchResultDiv.innerText = announceWinner();
                 
+                //resultDiv.appendChild(matchResultPara); 
+                
+                console.log("Displaying Match Result for " + (timeout / 1000) + " seconds");
                 // After X seconds of displaying result, reset the view
                 setTimeout(() => {
                     resultDiv.style.display = 'none';
@@ -169,16 +178,46 @@ function viewReset(){
                     resetButton.innerText = 'Reset';
                     buttonDiv.appendChild(resetButton);
                     resetButton.style.display = 'inline';
-                }, 3000);
+                    matchResultDiv.style.display = 'none'
+                }, timeout);
                 
                 // Wait for user to Reset the game
-                console.log("Resetting...");
+                //console.log("Resetting...");
                 resetButton.addEventListener('click', () => {
                     viewReset();
                 });
+                resetButton.addEventListener("mouseover", (event) => {
+                    console.log ("Moused-over: " + resetButton.className);
+                    //console.log (button.classlist);
+                    resetButton.className = resetButton.className + " playing";
+                });
+                resetButton.addEventListener("mouseout", (event) => {
+                    console.log ("Moused-out: " + resetButton.className);
+                    //console.log (button.classlist);
+                    if (resetButton.className.includes(" playing") ){
+                        resetButton.className = resetButton.className.slice(0, (resetButton.className.length - 8));
+                    }
+                });
+
             }
 
         });
+        button.addEventListener("mouseover", (event) => {
+            console.log ("Moused-over: " + button.className);
+            /*would like future functionality to show images on mouseover*/
+            //button.insertAdjacentHTML("afterbegin", "<img src=\"" + button.className + ".png\"/>");
+            //console.log("Inner HTML: "+button.innerHTML);
+            button.className = button.className + " playing";
+        });
+
+        button.addEventListener("mouseout", (event) => {
+            console.log ("Moused-out: " + button.className);
+            //console.log (button.classlist);
+            if (button.className.includes(" playing") ){
+                button.className = button.className.slice(0, (button.className.length - 8));
+            }
+        });
+
     });
 }
 
